d04945993d27bdb9d3b8d92aca61beedb58f175f
Christopher Wang <billybobjoethe2nd@aol.com>
2020-05-10 17:06:42 -0400

    Add RateLimit guard

---
+++ src/guards/RateLimit.ts
@@ -0,0 +1,20 @@
+import { Client } from '@typeit/discord';
+import { Message } from 'discord.js';
+
+export function RateLimit(options: { error_message?: string | null, rate_limit: number }) {
+    let last_message_time = 0;
+    options = Object.assign({
+        error_message: 'This command is on cooldown!'
+    }, options);
+
+    return (message: Message, client: Client) => {
+        const now = Date.now();
+        if ((now - last_message_time) / 1000 < options.rate_limit) {
+            options.error_message && message.reply(options.error_message);
+            return false;
+        }
+
+        last_message_time = now;
+        return true;
+    }
+}
