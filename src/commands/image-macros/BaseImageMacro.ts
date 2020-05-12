b6dedabf5e21814f0280b2e344462998bc4cbeac
Christopher Wang <billybobjoethe2nd@aol.com>
2020-05-12 00:56:31 -0400

    Fix image type

--- src/commands/image-macros/BaseImageMacro.ts
+++ src/commands/image-macros/BaseImageMacro.ts
@@ -15,7 +15,7 @@ export abstract class BaseImageMacro {
 
     public static font_cache = new Map<string, Promise<Font>>();
 
-    public static image_cache = new Map<string, Promise<any>>();
+    public static image_cache = new Map<string, Promise<Jimp>>();
 
     private static url_regex = /^<?((?:(?:http|https|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:((?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))))(?::\d{2,5})?(?:(?:\/|\?|#)[^\s]*?)?)>?$/i;
 
