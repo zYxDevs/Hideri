ec182c258bd000436ee772084ae59ae2210be1a6
Christopher Wang <billybobjoethe2nd@aol.com>
2020-05-11 22:50:34 -0400

    Add boorus

---
+++ src/commands/BooruCommand.ts
@@ -0,0 +1,72 @@
+import boorus from "../configs/boorus.json";
+import config from "../configs/config.json";
+import { Discord, CommandMessage } from '@typeit/discord';
+import { Command } from '../ArgumentParser';
+import { CommandGroup } from '../types/CommandGroup';
+import { forSite } from 'booru';
+import { MessageEmbed } from 'discord.js';
+
+export abstract class BaseBooruCommand {
+    public async exec(message: CommandMessage, booru: string, ...query: string[]) {
+        let booru_site;
+
+        try {
+            booru_site = forSite(booru);
+        } catch (e) {
+            return message.reply(`Booru \`${booru}\` does not exist.`);
+        }
+
+        query = query.map(segment => segment.replace(/\s+/g, '_'));
+        const results = await booru_site.search(query, { random: true });
+        if (results.posts.length == 0) return message.reply('Your query returned no results. (try adding wildcards)');            
+        const post = results.posts[0];
+
+        let title = post.source ?? post.tags;
+        if (Array.isArray(title)) title = title.join(' ');
+        if (title.length > 100) title = title.slice(0, 100) + '...';
+
+        const embed = new MessageEmbed({
+            url: post.postView,
+            title: title,
+            image: { url: post.fileUrl }
+        });
+
+        message.channel.send(embed);
+    }
+}
+
+@Discord({
+    prefix: config.prefix
+})
+export abstract class GenericBooruCommand extends BaseBooruCommand {
+    @Command('booru', {
+        group: CommandGroup.COMMUNITIES,
+        description: `Fetch an image from a booru`
+    })
+    public async exec(message: CommandMessage, booru: string, ...query: string[]) {
+        return await super.exec(message, booru, ...query);
+    }
+}
+
+boorus.forEach(({ name, booru, aliases, info, description } : {
+    name: string,
+    booru: string,
+    aliases?: string[],
+    info?: string,
+    description?: string
+}) => {
+    @Discord({
+        prefix: config.prefix
+    })
+    class BooruCommand extends BaseBooruCommand {
+        @Command(name, {
+            group: CommandGroup.COMMUNITIES,
+            aliases: aliases,
+            infos: info,
+            description: description ?? `Fetch an image from ${booru}`
+        })
+        public async exec(message: CommandMessage, ...query: string[]) {
+            return await super.exec(message, booru, ...query);
+        }
+    }
+});
