d371cb36c2659d58c2b1614d64c4f23b6c238637
Christopher Wang <billybobjoethe2nd@aol.com>
2020-06-02 23:23:27 -0400

    Add support for aliases and multiple images in imageemotes

--- src/commands/ImageEmoteCommand.ts
+++ src/commands/ImageEmoteCommand.ts
@@ -5,18 +5,20 @@ import { Command } from '../ArgumentParser';
 import { CommandGroup } from '../types/CommandGroup';
 import { MessageEmbed } from '../utils/EmbedUtils';
 import { get_prefix } from '../server-config/ServerConfig';
+import { RandomUtils } from '../utils/RandomUtils';
 
-image_emotes.forEach(({ name, info, description, url }) => {
+image_emotes.forEach(({ name, info, description, url, aliases }) => {
     @Discord(get_prefix)
     class ImageEmote {
         @Command(name, {
             infos: info,
             description: description,
-            group: CommandGroup.IMAGE_EMOTES
+            group: CommandGroup.IMAGE_EMOTES,
+            aliases: aliases
         })
         private async exec(message: CommandMessage) {
             const embed = new MessageEmbed();
-            embed.setImage(url);
+            embed.setImage(Array.isArray(url) ? RandomUtils.choice(url) : url);
             message.channel.send(embed);
         }
     }
