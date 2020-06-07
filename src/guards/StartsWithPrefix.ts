0b18bdfe56b82be9f285f97f0f47f8ab94d87fd9
Christopher Wang <billybobjoethe2nd@aol.com>
2020-06-07 14:05:10 -0400

    Check for mention/channel/group in StartsWithPrefix

--- src/guards/StartsWithPrefix.ts
+++ src/guards/StartsWithPrefix.ts
@@ -1,4 +1,9 @@
 import { GuardFunction } from '@typeit/discord';
 import { get_prefix_str } from '../server-config/ServerConfig';
 
-export const StartsWithPrefix: GuardFunction<'message'> = ([message], client, next) => message.content.startsWith(get_prefix_str(message)) && next()
+export const StartsWithPrefix: GuardFunction<'message'> = ([message], client, next) => {
+    if (message.content.startsWith(get_prefix_str(message)) &&
+        !/^<(?:@[!&]|#|:\S{1,32}:)\d{17,20}>/.test(message.content)) {
+        next();
+    }
+}
