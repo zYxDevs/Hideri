24ed9fd29b20ed5615016b7db6dd79f6dc12af80
Christopher Wang <billybobjoethe2nd@aol.com>
2020-06-09 21:59:06 -0400

    Add trap command

--- src/commands/ImageCommand.ts
+++ src/commands/ImageCommand.ts
@@ -1,10 +1,10 @@
 import { Command } from '../ArgumentParser';
 import { Discord, CommandMessage } from '@typeit/discord';
-import config from '../configs/config.json';
 import fetch from 'node-fetch';
 import { CommandGroup } from '../types/CommandGroup';
-import { EmbedUtils } from '../utils/EmbedUtils';
+import { EmbedUtils, MessageEmbed } from '../utils/EmbedUtils';
 import { get_prefix } from '../server-config/ServerConfig';
+import { nekos } from '../apis/Instances';
 
 @Discord(get_prefix)
 export abstract class ImageCommand {
@@ -25,5 +25,24 @@ export abstract class ImageCommand {
     })
     private async hibiki(message: CommandMessage) {
         message.channel.send(EmbedUtils.create_image_embed('Hibiki', 'https://cdn.discordapp.com/attachments/713123662085226587/714322327055368263/ev303.png'));
+    }
+
+    @Command('trap', {
+        infos: 'Get a random trap',
+        group: CommandGroup.IMAGE_EMOTES,
+        aliases: [
+            'josou',
+            'femboy',
+            'otoko',
+            'otokonoko',
+            'htrap',
+            'hfemboy'
+        ],
+        nsfw: true
+    })
+    private async trap(message: CommandMessage) {
+        const { url } = Math.random() > .5 ? await (await fetch('https://api.computerfreaker.cf/v1/trap')).json() : await nekos.nsfw.trap();
+
+        return message.channel.send(new MessageEmbed({ image: { url: url } }));
     }
 }
