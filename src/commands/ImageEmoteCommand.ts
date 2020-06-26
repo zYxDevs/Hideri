53a1892d9ab8eee37f499f1db728138537b06480
Christopher Wang <billybobjoethe2nd@aol.com>
2020-06-26 19:24:02 -0400

    Add support for nsfw image emote commands

--- src/commands/ImageEmoteCommand.ts
+++ src/commands/ImageEmoteCommand.ts
@@ -7,14 +7,15 @@ import { MessageEmbed } from '../utils/EmbedUtils';
 import { get_prefix } from '../server-config/ServerConfig';
 import { RandomUtils } from '../utils/RandomUtils';
 
-image_emotes.forEach(({ name, info, description, url, aliases, raw }) => {
+image_emotes.forEach(({ name, info, description, url, aliases, raw, nsfw }) => {
     @Discord(get_prefix)
     class ImageEmote {
         @Command(name, {
             infos: info,
             description: description,
             group: CommandGroup.IMAGE_EMOTES,
-            aliases: aliases ?? []
+            aliases: aliases ?? [],
+            nsfw: nsfw ?? false
         })
         private async exec(message: CommandMessage) {
             const image = Array.isArray(url) ? RandomUtils.choice(url) : url;
